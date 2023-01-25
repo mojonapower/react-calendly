b83b19abb9d4be7d998ecb4b6896f9a8b463d0fb
mojonapower <maca95@gmail.com>
2023-01-25 20:24:58 +0000

    componente para la cata

--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import Cata from "./Cata"
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Cata/>
   </React.StrictMode>
 );
 
